0693067a2c6c49e1f2af424a6d9335c4e1d5b4fc
Marco Rebhan <me@dblsaiko.net>
2021-08-03 22:02:33 +0200

    Update to 1.17.1

--- src/main/java/net/dblsaiko/bruhmoment/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/net/dblsaiko/bruhmoment/mixin/ServerPlayNetworkHandlerMixin.java
@@ -1,54 +1,57 @@
 package net.dblsaiko.bruhmoment.mixin;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.Vec3d;
 
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.dblsaiko.bruhmoment.util.Util;
 
-@Mixin(ServerPlayNetworkHandler.class)
+// we need to beat fabric api calling the interact event
+@Mixin(targets = "net/minecraft/server/network/ServerPlayNetworkHandler$1", priority = 999)
 public class ServerPlayNetworkHandlerMixin {
+    @Shadow
+    public ServerPlayNetworkHandler field_28963;
 
-    @Shadow @Final private MinecraftServer server;
-
-    @Shadow public ServerPlayerEntity player;
+    @Shadow
+    public Entity field_28962;
 
     @Inject(
-        method = "onPlayerInteractEntity(Lnet/minecraft/network/packet/c2s/play/PlayerInteractEntityC2SPacket;)V",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/server/network/ServerPlayerEntity;squaredDistanceTo(Lnet/minecraft/entity/Entity;)D",
-            shift = Shift.BEFORE
-        ),
+        method = "interact(Lnet/minecraft/util/Hand;)V",
+        at = @At(value = "HEAD"),
         cancellable = true
     )
-    private void onInteract(PlayerInteractEntityC2SPacket rpacket, CallbackInfo ci) {
-        ServerWorld serverWorld = this.player.getServerWorld();
-        Entity entity = rpacket.getEntity(serverWorld);
-        switch(rpacket.getType()) {
-            case INTERACT:
-            case INTERACT_AT:
-                if (!Util.canInteract(this.player, entity, rpacket.getHand())) {
-                    ci.cancel();
-                }
-                break;
-            case ATTACK:
-                if (!Util.canAttack(this.player, entity)) {
-                    ci.cancel();
-                }
-                break;
+    public void bmInteract(Hand hand, CallbackInfo ci) {
+        if (!Util.canInteract(this.field_28963.player, this.field_28962, hand)) {
+            ci.cancel();
         }
     }
 
+    @Inject(
+        method = "interactAt(Lnet/minecraft/util/Hand;Lnet/minecraft/util/math/Vec3d;)V",
+        at = @At(value = "HEAD"),
+        cancellable = true
+    )
+    public void bmInteractAt(Hand hand, Vec3d pos, CallbackInfo ci) {
+        if (!Util.canInteract(this.field_28963.player, this.field_28962, hand)) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(
+        method = "attack()V",
+        at = @At(value = "HEAD"),
+        cancellable = true
+    )
+    public void bmAttack(CallbackInfo ci) {
+        if (!Util.canAttack(this.field_28963.player, this.field_28962)) {
+            ci.cancel();
+        }
+    }
 }
